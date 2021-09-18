c23c33e61a7d0578ef4f00f263b1c8d950206642
Robert Isaacs <robertisaacs87@yahoo.com>
2021-09-18 14:27:16 -0500

    setting up questions in script

---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+// using arraings for number, questions, options, and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hippie Test Melodrama Lounge",
+            "Hyper Tool Multi Language"
+        ]
+    }
+]
