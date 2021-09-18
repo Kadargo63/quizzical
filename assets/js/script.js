29f9c63ad124c42fba48f3dae9dcd5481e9deafd
Robert Isaacs <robertisaacs87@yahoo.com>
2021-09-18 18:04:11 -0500

    added js to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,79 @@ const time_line = document.querySelector(".header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
+// if start button clicked
+start_btn.onlick = ()=>{
+    info_box.classList.add("activeInfo");
+}
+
+// if exit button clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+} 
+
+// if continue button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
+    showQuestions(0);
+    queCounter(1);
+    startTimer(15);
+    startTinmerLine(0);
+}
+
+let timeValue = 15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restart button clicked
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz");
+    timeValue = 15;
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuestions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    clearInterval(counterLine);
+    startTimer(widthValue);
+    timeText.textContent = "time Left";
+    next_btn.classList.remove("show");
+}
+
+// if quit button clicked
+quit_quiz.onclick = ()=>{
+    window.location.reload(; )
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .toal_que");
+
+// if Next button clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){
+        que_count++;
+        que_numb++;
+        showQuestions(queCount);
+        queCounter(que_numb);
+        clearInterval(counter);
+        clearInterval(counterLine);
+        startTimer(timeValue);
+        startTimerLine(widthValue);
+        timeText.textContent = "Time Left";
+        next_btn.classList.remove("show");
+    }else{
+        clearInterval(counter);
+        clearInterval(counterLine);
+        showResult();
+    }
+}
+
