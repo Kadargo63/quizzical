96f351e95ee1ee3ebf1459fc6a05809b66003489
Robert Isaacs <robertisaacs87@yahoo.com>
2021-09-18 14:52:33 -0500

    added 6 quiz questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -11,5 +11,60 @@ let questions = [
             "Hippie Test Melodrama Lounge",
             "Hyper Tool Multi Language"
         ]
-    }
-]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Comforter/Sheet Set",
+            "Cascading Style Sheet",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hyperpoloidal Prepropaganda",
+            "Homeopathic Processing Power",
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Stonewall Queery Language",
+            "Structured Query Language",
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "xenophobic marketing language",
+            "eXtension multiplicity language",
+            "x-ray meat locker"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Who framed Roger Rabbit?",
+        answer: "Judge Doom",
+        options: [
+            "Eddie Valiant",
+            "Judge Doom",
+            "Jessica Rabbit",
+            "R. K. Maroon",
+        ]
+    },
+];
