c9f276be2591aa95e71a8fb2ca523a4628e8578b
Robert Isaacs <robertisaacs87@yahoo.com>
2021-09-19 11:25:56 -0500

    added functions for selecting answer options and showing results

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ restart_quiz.onclick = ()=>{
 
 // if quit button clicked
 quit_quiz.onclick = ()=>{
-    window.location.reload(; )
+    window.location.reload();
 }
 
 const next_btn = document.querySelector("footer .next_btn");
@@ -86,3 +86,77 @@ next_btn.onclick = ()=>{
     }
 }
 
+// getting questions and options from array
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+
+    //create a new span and div tag for a question and option and pass the value using array index
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question + '<span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>' + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>' 
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+
+    const option = option_list.querySelectorAll("option");
+
+    //set onclick attribute to all available options
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionsSelected(this)");
+    }
+}
+
+// create new div tags
+let tickIconTag = '<div class="icon tick"><i class="fas fa_check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div';
+
+// if user clicks on option
+function optionSelected(answer){
+    clearInterval(counter); 
+    clearInterval(counterLine);
+    let userAns = answer.textContent;
+    let correcAns = questions[que_count].answer;
+    const allOptions = option_list.children.length;
+
+    if(userAns == correcAns){
+        userScore += 1;
+        answer.classList.add("correct");
+        answer.insertAdjacentHTML("beforeend", tickIconTag);
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend", crossIconTag);
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){
+                option_list.children[i].setAttribute("class", "option correct");
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled");
+    }
+    next_btn.classList.add("show");
+}
+
+function showResults(){
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.remove("activeQuiz");
+    result_box.classList.add("ativeResult");
+    const scoreText = result_box.querySelector(".score_text");
+    if (userScore > 3){
+        let scoreTag = '<span>and congrats! You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 1){
+        let scoreTag = '<span>and nice, You got <p>'+ userScore +'<p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{
+        let scoreTag = '<span>and wow this is awkward, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
